d28a6065d227ce6d3c33d983c527a2677d9c0439
ArijSaaleh <arij.saleh.pro@gmail.com>
2025-08-17 11:08:18 +0100

    test commit

--- src/main/java/com/arij/fashionecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/com/arij/fashionecommerce/exception/GlobalExceptionHandler.java
@@ -28,7 +28,7 @@ public class GlobalExceptionHandler {
                 .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
     }
-
+// testing comment
     // Any other unhandled exception
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGeneral(Exception ex) {
